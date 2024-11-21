2ee33e26276eb69d810e926163bf81e82f26c868
Arthur Salviano <arthursalviano7@gmail.com>
2024-11-20 22:51:01 -0300

    Host change

--- server/src/services/db.js
+++ server/src/services/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2');
 require("dotenv").config()
 
 const conn = mysql.createConnection({
-    host: "localhost",
+    host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE,
